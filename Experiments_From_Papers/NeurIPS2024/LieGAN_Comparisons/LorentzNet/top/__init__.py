ca6ee4ecef02638862cf7fb1408365fec2f06fc5
benshaw2 <bendougshaw@gmail.com>
2024-10-29 23:56:12 -0600

    Finalize Revised Similarity Scores

--- Experiments_From_Papers/NeurIPS2024/LieGAN_Comparisons/LorentzNet/top/__init__.py
+++
@@ -1,4 +0,0 @@
-from .jetdatasets import JetDataset
-from .utils import initialize_datasets, randomSO13pTransform, randomLieGroupTransform
-from .collate import collate_fn
-from .dataset import retrieve_dataloaders
